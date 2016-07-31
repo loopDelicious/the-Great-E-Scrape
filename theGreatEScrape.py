6345e3143f80c430c96c7eeb31ea33daac4b7494
Joyce Lin <joycelin79@gmail.com>
2016-07-31 16:04:14 -0700

    handle httpeerror and urlerror

--- theGreatEScrape.py
+++ theGreatEScrape.py
@@ -4,16 +4,31 @@ from bs4 import BeautifulSoup
 # import the library used to query a website
 import urllib2
 
-# open and read the website
-pageFile = urllib2.urlopen("http://candiceswanepoeldaily.tumblr.com/")
-pageHtml = pageFile.read()
-pageFile.close()
 
-# call BeautifulSoup on an array of lines in string format
-soup = BeautifulSoup("".join(pageHtml))
 
-# find all links in array of lines
-hrefs = soup.findAll("a")
+try:
+    # open and read the website
+    pageFile = urllib2.urlopen("http://candiceswanepoeldaily.tumblr.com/")
+    pageHtml = pageFile.read()
+    pageFile.close()
 
-for href in hrefs:
-    print href
+    # call BeautifulSoup on an array of lines in string format
+    soup = BeautifulSoup("".join(pageHtml))
+
+    # find all links in array of lines
+    hrefs = soup.findAll("a")
+
+    for href in hrefs:
+        print href
+
+except urllib2.URLError as e:
+    # exception handling for URLError
+    if hasattr(e, 'reason'):
+        print "We failed to reach a server."
+        print "Reason: ", e.reason
+    # exception handling for HTTPError
+    elif hasattr(e, 'code'):
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code; ', e.code
+    else:
+        print 'Everything is fine.'
